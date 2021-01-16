a778bff829f3011d8bd5aa5b82980f61b051c43b
victor-jml <zyf695112338@qq.com>
2021-01-16 18:24:34 +0800

    fixed

--- src/ds_algo/AcWing/wintervacation/WarehouseLocation104.java
+++ src/ds_algo/AcWing/wintervacation/WarehouseLocation104.java
@@ -26,7 +26,7 @@ public class WarehouseLocation104 {
     public int solution(int[] arr){
         int res = 0;
         for(int i = 0 ; i < arr.length ; i++){
-            // calculate the arr[i] - median 
+            // calculate the arr[i] - median
             res += Math.abs(arr[i] - arr[arr.length / 2]);
         }
         return res;
