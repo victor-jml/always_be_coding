3d9fa87e33e1879d9ec848a7ab5ca33bfd361850
victor-jml <zyf695112338@qq.com>
2021-02-02 17:47:50 +0800

    [AcWing] 1432.BoardChallenge

--- src/ds_algo/AcWing/wintervacation/BoardChallenge1432.java
+++ src/ds_algo/AcWing/wintervacation/BoardChallenge1432.java
@@ -11,16 +11,71 @@ import java.util.Scanner;
  * https://www.acwing.com/problem/content/1434/
  */
 public class BoardChallenge1432 {
-    List<int[]> res = new ArrayList<>();
+    private static List<List<Integer>> res = new ArrayList<>();
+    private static List<Integer> path = new ArrayList<>();
+    private static int[][] arr;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] arr = new int[n][n];
-        dfs(arr,0);
+        arr = new int[n][n];
+        dfs(0);
+        for(int i = 0 ; i < 3 ; i++){
+            List<Integer> list = res.get(i);
+            for(int x : list){
+                System.out.print(x + " ");
+            }
+        }
     }
 
-    private static void dfs(int[][] arr ,int index){
-        
+    /**
+     *
+     * @param index row
+     */
+    private static void dfs(int index){
+        if(index == arr.length ){
+            List<Integer> path = new ArrayList<>();
+            for(int i = 0 ; i < arr.length ; i++){
+                StringBuilder sb = new StringBuilder();
+                for(int j = 0; j < arr.length ;j++){
+                    if(arr[j][i] == 1){
+                        sb.append()
+                    }
+                }
+            }
+        }
+        // loop col
+        for(int i = 0 ; i < arr.length ; i++){
+            if(check(i,index)){
+                arr[i][index] = 1;
+                dfs(index + 1);
+                arr[i][index] = 0;
+            }
+        }
     }
 
+    private static boolean check(int x,int y){
+        int x1 = x;
+        int y1 = y;
+        int n = arr.length;
+        for(int i = x1 ; i >= 0; i--){
+            if(arr[i][y1] == 1){
+                return false;
+            }
+        }
+        x1 = x;
+        y1 = y;
+        while(x1 >= 0 && y1 >= 0){
+            if(arr[--x1][--y1] == 0){
+                return false;
+            }
+        }
+        x1 = x;
+        y1 = y;
+        while (x1 < n && y1 < n){
+            if(arr[++x1][--y1] == 1){
+                return false;
+            }
+        }
+        return true;
+    }
 }
